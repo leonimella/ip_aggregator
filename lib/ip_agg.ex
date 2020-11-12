6614777dd17c93599ef9866de67835e9d8130731
Leoni Mella <leoni.mella@gmail.com>
2020-11-12 16:26:26 -0300

    Finished exercise

--- lib/ip_agg.ex
+++ lib/ip_agg.ex
@@ -5,21 +5,23 @@ defmodule IpAgg do
   use GenServer
 
   @impl true
-  def init(stack) do
-    {:ok, stack}
+  def init(_stack) do
+    {:ok, %{}}
   end
 
   @impl true
-  def handle_call(:pop, _from, [head | tail]) do
-    {:reply, head, tail}
-  end
-
   def handle_call(:get_state, _from, state) do
     {:reply, state, state}
   end
 
   @impl true
-  def handle_cast({:push, element}, state) do
-    {:noreply, [element | state]}
+  def handle_cast({:push, ip_list}, state) do
+    {
+      :noreply,
+      Enum.reduce(ip_list, state, fn ip, acc ->
+        acc
+        |> Map.update(ip, 1, fn ip_count -> ip_count + 1 end)
+      end)
+    }
   end
 end
